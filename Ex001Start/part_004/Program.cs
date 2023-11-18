5775069c8878e4fac37f7cf1827a94409bad9c85
Const18 <Const18github@ogorodov.udm.ru>
2023-11-18 14:00:44 +0400

    Изменение структуры

---
+++ Ex001Start/part_004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Сколько элементов массива нужно: ");
+
+int count = int.Parse(Console.ReadLine());
+int[] array = new int[count];
+
+int index = 0;
+while (index < count) {
+    array[index] = Random.Shared.Next(0, 10); // [0, 10)
+    Console.Write($"{array[index]} ");
+    index += 1;
+}
+
+Console.WriteLine();
