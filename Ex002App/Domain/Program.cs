54ff493f9cb846f04eb6cd3b809ce1d4259378a5
Const18 <Const18github@ogorodov.udm.ru>
2023-11-25 20:54:07 +0400

    Занятие 25.11.2023

--- Ex002App/Domain/Program.cs
+++ Ex002App/Domain/Program.cs
@@ -1,9 +1,39 @@
 ﻿using ContactApplication;
+//using System.Collections.Generics;
 
 class Program
 {
   static void Main(string[] args)
   {
-    App.Run();
+    // App.Run();
+
+    Dictionary<string, string> dic = new();
+
+dic.Add("up", "вверх");
+dic["down"]= "вниз";
+
+var keys = dic.Keys;
+foreach (string item in keys) Console.WriteLine(item);
+
+var values = dic.Values;
+foreach (string item in values) Console.WriteLine(item);
+
+string value = dic["up"];
+Console.WriteLine(value);
+Console.WriteLine($"dic[\"up\"]= {dic["up"]}");
+
+Console.WriteLine(dic.ContainsKey("down"));
+Console.WriteLine(dic.ContainsKey("left"));
+
+if (dic.ContainsKey("up"))
+{
+  dic["up"] = "верх";
+}
+else
+{
+  dic.Add("up", "вверх");
+}
+
+Console.WriteLine($"dic[\"up\"]= {dic["up"]}");
   }
 }
---
+++ Ex003Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.IO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var fs = new StreamReader("numbers.txt");
+        int sum = 0;
+        while(!fs.EndOfStream)
+        {
+            int v = int.Parse(fs.ReadLine());
+            if(v % 3 == 0)
+                sum += v;
+        }
+        Console.WriteLine(sum);
+        fs.Close();
+
+        Console.WriteLine(File.ReadAllLines("numbers.txt").Select(int.Parse).Where(x => x % 3 == 0).Sum());
+    }
+}
